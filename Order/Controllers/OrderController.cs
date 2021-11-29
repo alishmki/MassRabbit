97e96e5af6bd72b41fc1ec8567b0346bab05d505
Ali Shamekhi <shamekhi.ali@gmail.com>
2021-11-29 08:10:15 +0000

    'init'

---
+++ Order/Controllers/OrderController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MassTransit;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Model;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace OrderService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly IPublishEndpoint publishEndpoint;
+        private readonly ILogger<OrderController> logger;
+
+        public OrderController(IPublishEndpoint publishEndpoint, ILogger<OrderController> logger)
+        {
+            this.publishEndpoint = publishEndpoint;
+            this.logger = logger;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Order order)
+        {
+            await publishEndpoint.Publish<Order>(order);
+            logger.LogInformation("order sent!");
+            return Ok();
+        }
+    }
+}
